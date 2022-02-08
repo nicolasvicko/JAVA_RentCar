d21b8e1c1989361d034306c5a00d83716010ab72
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2022-02-08 14:30:32 -0300

    Add files via upload
    atualizando o projeto

---
+++ src/model/entities/Invoice.java
@@ -0,0 +1,38 @@
+package model.entities;
+
+public class Invoice {
+	
+	private Double basicPayment;
+	private Double Tax;
+	
+	public Invoice() {
+		
+	}
+
+	public Invoice(Double basicPayment, Double tax) {
+		
+		this.basicPayment = basicPayment;
+		Tax = tax;
+	}
+
+	public Double getBasicPayment() {
+		return basicPayment;
+	}
+
+	public void setBasicPayment(Double basicPayment) {
+		this.basicPayment = basicPayment;
+	}
+
+	public Double getTax() {
+		return Tax;
+	}
+
+	public void setTax(Double tax) {
+		Tax = tax;
+	}
+	
+	public Double getTotalPayment() {
+		return getBasicPayment() + getTax();
+	}
+
+}
